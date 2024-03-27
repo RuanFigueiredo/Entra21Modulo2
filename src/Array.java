ac589bc8a098a70fdf61e581d38fd14099d103f7
RuanFigueiredo <ruanthiago67@gmail.com>
2024-03-27 08:54:06 -0300

    arrays com loop

---
+++ src/Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main (String[] args){
+        Scanner entrada  = new Scanner(System.in);
+
+        int[] notas = new int [3];
+
+        double media;
+
+        for(int i = 0;i < notas.length; i++){
+            System.out.println("digite uma nota: ");
+            notas[i] = entrada.nextInt();
+        }
+
+        for(int i = 0; i < notas.length;i++){
+            System.out.println(notas[i]);
+        }
+
+    }
+}
