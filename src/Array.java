a564fee78251d14fb926e0a1bea54b819238515c
RuanFigueiredo <ruanthiago67@gmail.com>
2024-03-28 16:20:27 -0300

    trabalhando com arrays

--- src/Array.java
+++ src/Array.java
@@ -2,20 +2,29 @@ import java.util.Scanner;
 
 public class Array {
     public static void main (String[] args){
-        Scanner entrada  = new Scanner(System.in);
+        Scanner scan  = new Scanner(System.in);
 
-        int[] notas = new int [3];
+        //vetores
+        int[] notas = new int [3]; // vetor notas com 3 posições
+        int tamNotas = notas.length;
+        /*
+        notas[0] = 10; // 1ª posição
+        notas[1] = 8; // 2ª posição
+        notas[2] = 9; // 3ª posição
 
-        double media;
+        //visualisação de dados
+        System.out.println("1ª nota: " + notas[0]);
+        System.out.println("2ª nota: " + notas[1]);
+        System.out.println("3ª nota: " + notas[2]);*/
 
-        for(int i = 0;i < notas.length; i++){
-            System.out.println("digite uma nota: ");
-            notas[i] = entrada.nextInt();
+        //inserção com FOR
+        for(int i =0; i < tamNotas; i++){
+            System.out.println((i+1)+"ª Nota: ");
+            notas[i] = scan.nextInt();
         }
 
-        for(int i = 0; i < notas.length;i++){
-            System.out.println(notas[i]);
-        }
+
+
 
     }
 }
